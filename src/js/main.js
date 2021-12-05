12bec4880c5034f90ce0cdbf2ee5b64ecc8f7141
fLaMEd <shadybrothers@gmail.com>
2021-12-05 14:54:48 +1300

    restyled the navigation from scratch to create a vertical responsive navigation menu with mobile toggle button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
-function openNav() {
-  document.getElementById("myNav").style.display = "block";
-  document.getElementByID("toggleNav").style.display = "none";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.display = "none";
-  document.getElementById("toggleNav").style.display = "block";
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
