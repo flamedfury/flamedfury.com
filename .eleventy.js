2cb2e1e6548566eaac1a25da0b59aeea6818dce6
fLaMEd <shadybrothers@gmail.com>
2021-12-11 15:39:46 +1300

    fix relative path for imgs that are removed from the sites root. added css/html validation badges

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addWatchTarget("./src/sass/");
-
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/js");
+
+  eleventyConfig.addFilter(
+    "relative",
+    (page, root = "/") =>
+      `${require("path").relative(page.filePathStem, root)}/`
+  );
 
   // set the date once per build to avoid different results for different calls
   let date;
@@ -15,7 +19,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addShortcode("year", () => `${date.getFullYear()}`);
-  
+
   // required for the _recentlyread api call
   eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
 
