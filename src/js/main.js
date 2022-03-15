f32b59e96253641289c14c5c248267cd3b1781aa
flamedfury <shadybrothers@gmail.com>
2022-03-15 22:00:13 +1300

    re-add nav toggle to main.js file as nav was broken on mobile, whoops

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
-function openNav() {
-  document.getElementById("myNav").style.display = "block";
-  document.getElementByID("toggleNav").style.display = "none";
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
-
-function closeNav() {
-  document.getElementById("myNav").style.display = "none";
-  document.getElementById("toggleNav").style.display = "block";
-} 
