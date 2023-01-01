1154ed4870db995e78c892d3603239712ee0a42d
fLaMEd <heavy.job8629@fastmail.com>
2023-01-01 15:49:06 +1300

    enhancing lastfm grid styles

--- src/_data/lastfm.js
+++ src/_data/lastfm.js
@@ -8,7 +8,7 @@ const API = 'http://ws.audioscrobbler.com/2.0/'
 module.exports = async () => {
   try {
     const json = await Cache(
-      `${API}?method=user.getrecenttracks&user=${USERNAME}&limit=9&api_key=${API_KEY}&format=json`,
+      `${API}?method=user.getrecenttracks&user=${USERNAME}&limit=6&api_key=${API_KEY}&format=json`,
       {
         duration: '2h',
         type: 'json',
