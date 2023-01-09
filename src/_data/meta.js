65018f9a1e299183695de60bdabd89aff7632af9
fLaMEd <heavy.job8629@fastmail.com>
2023-01-09 20:04:10 +1300

    update site meta file

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -4,6 +4,7 @@ module.exports = {
   pkv: package.version || 'v1',
   url: process.env.URL || 'http://localhost:8080',
   siteName: 'Flamed Fury',
+  siteVersion: '2302',
   siteDescription:
     'My personal homepage, full of stuff that means everything to me, and nothing to you. You will find my thoughts about the current state of the web, my recollections of my earliest memories of the web, and a bunch of pages detailing some of my interests in records, books, comics, games, movies and tv shows. Come have a read and leave me a message.',
   siteType: 'Person', // schema
@@ -13,38 +14,21 @@ module.exports = {
   toc: 'Table of contents',
   skipToc: 'Skip table of contents',
   author: 'fLaMEd', // i.e. Lene Saile - author's name. Must be set.
-  authorEmail: '', // i.e. hola@lenesaile.com - email of the author
-  authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
-  themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
+  authorEmail: 'flamed@flamedfury.com', // i.e. hola@lenesaile.com - email of the author
+  authorWebsite: 'https://flamedfury.com', // i.e. https.://www.lenesaile.com - the personal site of the author
+  themeColor: '#f05123', //  Manifest: defines the default theme color for the application
+  themeBgColor: '#252525', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg',
-    twitterSite: '', // i.e. @site - twitter profile of the site
-    twitterCreator: '', // i.e. @author -  twitter profile of the site
     mastodonProfile: '' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
   },
   blog: {
     // this is for the rss feed
-    name: 'My great Web Development Blog',
+    name: 'fLaMEdFURY :: my collection of stuff',
     description:
-      'Tell the word what you are writing about in your blog! It will show up on feed readers.'
+      'My personal homepage, full of stuff that means everything to me, and nothing to you. You will find my thoughts about the current state of the web, my recollections of my earliest memories of the web, and a bunch of pages detailing some of my interests in records, books, comics, games, movies and tv shows. Come have a read and leave me a message.'
   },
   pagination: {
     itemsPerPage: 20
-  },
-  address: {
-    // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
-    firma: 'Flamed Fury',
-    street: '',
-    city: '',
-    state: '',
-    zip: '',
-    mobileDisplay: '',
-    mobileCall: '',
-    email: 'flamed@flamedfury.com',
-    cif: ''
-  },
-  menu: {
-    closedText: 'Menu'
   }
 };
