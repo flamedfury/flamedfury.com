9a797f2752dd5c730b1d3e11cfadb1fe4827f7c9
flamedfury <heavy.job8629@fastmail.com>
2023-03-17 12:36:15 +1300

    fix output dir for generated images

--- config/shortcodes/image/index.js
+++ config/shortcodes/image/index.js
@@ -1,17 +1,17 @@
 const Image = require('@11ty/eleventy-img');
 const path = require('path');
+const htmlmin = require('html-minifier-terser');
 
-const imageShortcode = async (src, pcls, cls, alt, sizes = '100vw') => {
+const imageShortcodePlaceholder = async (src, alt, caption, sizes = '100vw') => {
   if (!alt) {
-    throw new Error(`Missing \`alt\` on Image from: ${src}`);
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
   }
 
   let metadata = await Image(src, {
     widths: [400, 700, 1280],
     formats: ['avif', 'webp', 'jpeg'],
     urlPath: '/assets/images/',
-    outputDir: './dist/assets/images/',
-    // Custom Image Filename
+    outputDir: './dist/assets/images/generated',
     filenameFormat: function (id, src, width, format, options) {
       const extension = path.extname(src);
       const name = path.basename(src, extension);
@@ -22,7 +22,18 @@ const imageShortcode = async (src, pcls, cls, alt, sizes = '100vw') => {
 
   let lowsrc = metadata.jpeg[0];
 
-  return `<picture class="${pcls}">
+  // getting the url  to use
+  let imgSrc = src;
+  if (!imgSrc.startsWith('.')) {
+    const inputPath = this.page.inputPath;
+    const pathParts = inputPath.split('/');
+    pathParts.pop();
+    imgSrc = pathParts.join('/') + '/' + src;
+  }
+
+  return htmlmin.minify(
+    `<figure>
+     <picture>
     ${Object.values(metadata)
       .map(imageFormat => {
         return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat
@@ -31,12 +42,22 @@ const imageShortcode = async (src, pcls, cls, alt, sizes = '100vw') => {
       })
       .join('\n')}
       <img
-        src="${lowsrc.url}"
-        class="${cls}"
+        src="/assets/images/image-placeholder.png"
+        data-src="${lowsrc.url}"
         width="${lowsrc.width}"
         height="${lowsrc.height}"
-        alt="${alt}">
-    </picture>`;
+        alt="${alt}"
+				loading = 'lazy'
+        decoding="async">
+    </picture>
+    ${caption
+      ? `<figcaption class="cluster font-display"><p>${caption}</p>
+	</figcaption>`
+      : ``
+    }
+</figure>`,
+    { collapseWhitespace: true }
+  );
 };
 
-module.exports = imageShortcode;
+module.exports = imageShortcodePlaceholder;
