c4f29e3497f45293dfa918f168b04eb4aab61b8b
flamedfury <heavy.job8629@fastmail.com>
2023-06-17 19:30:23 +1200

    update games.js to retrieve coverart for each game played to display on the now page

--- src/_data/games.js
+++ src/_data/games.js
@@ -4,14 +4,40 @@ require('dotenv').config({ systemvars: true });
 module.exports = async function () {
   const API_KEY = process.env.STEAM_KEY;
   const STEAMID = process.env.STEAM_ID;
-  const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${API_KEY}&steamid=${STEAMID}&format=json`
-  const res = EleventyFetch(url, {
+  const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${API_KEY}&steamid=${STEAMID}&format=json`;
+  const res = await EleventyFetch(url, {
     duration: '1h',
     type: 'json',
-  })
-  const game = await res
-  return game.response.games
-}
+  });
+  const games = res.response.games;
+
+  const gamesWithCoverArt = await Promise.all(
+    games.map(async (game) => {
+      const gameDetailsUrl = `http://store.steampowered.com/api/appdetails?appids=${game.appid}`;
+      const gameDetailsRes = await EleventyFetch(gameDetailsUrl, {
+        duration: '1h',
+        type: 'json',
+      });
+
+      const gameDetails = gameDetailsRes[game.appid].data;
+      if (gameDetails && gameDetails.hasOwnProperty('header_image')) {
+        const coverArtUrl = gameDetails.header_image;
+        const coverArtRes = await fetch(coverArtUrl);
+        const coverArtBuffer = await coverArtRes.arrayBuffer();
+        const coverArtArray = Array.from(new Uint8Array(coverArtBuffer));
+        const coverArtBase64 = Buffer.from(coverArtArray).toString('base64');
+        game.coverArt = `data:image/png;base64,${coverArtBase64}`;
+      } else {
+        game.coverArt = null;
+      }
+
+      return game;
+    })
+  );
+
+  return gamesWithCoverArt;
+};
+
 
 // returns
 // appid:
