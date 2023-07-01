090b0d539e00ffe12a7debd9d82bc877c9f6a563
flamedfury <heavy.job8629@fastmail.com>
2023-07-01 14:24:01 +1200

    now page: update tv.js to add in api.tmdb.org call for images

--- src/_data/tv.js
+++ src/_data/tv.js
@@ -2,6 +2,7 @@ const EleventyFetch = require('@11ty/eleventy-fetch')
 
 module.exports = async function () {
   const TV_KEY = process.env.TRAKT_API_KEY
+  const TMDB_KEY = process.env.TMDB_API_KEY
   const url = 'https://api.trakt.tv/users/shadybrothers/history/shows'
   const res = await EleventyFetch(url, {
     duration: '1h',
@@ -14,6 +15,18 @@ module.exports = async function () {
       },
     },
   })
-  const shows = res
-  return shows.slice(0, 6)
+  const shows = res.slice(0, 4)
+
+  for (const show of shows) {
+    const tmdbId = show.show.ids.tmdb
+    const tmdbUrl = `https://api.themoviedb.org/3/tv/${tmdbId}?api_key=${TMDB_KEY}`
+    const tmdbRes = await EleventyFetch(tmdbUrl, {
+      duration: '1h',
+      type: 'json',
+    })
+    const posterPath = tmdbRes.poster_path
+    show.posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`
+  }
+
+  return shows
 }
