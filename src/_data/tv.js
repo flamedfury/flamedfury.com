6d46c48264e640be887e65e2464859829c0edffe
flamedfury <heavy.job8629@fastmail.com>
2023-07-01 13:51:23 +1200

    now page: update tv.js to use api.trakt.tv. update movies.js to add in api.tmdb.org call for images

--- src/_data/tv.js
+++ src/_data/tv.js
@@ -1,13 +1,19 @@
-const { extract } = require('@extractus/feed-extractor')
-const { AssetCache } = require('@11ty/eleventy-fetch')
+const EleventyFetch = require('@11ty/eleventy-fetch')
 
 module.exports = async function () {
-  const TV_KEY = process.env.TRAKT_KEY
-  const url = `https://trakt.tv/users/shadybrothers/history/shows/added.atom?slurm=${TV_KEY}`
-  const asset = new AssetCache('tv_data')
-  if (asset.isCacheValid('1h')) return await asset.getCachedValue()
-  const res = await extract(url).catch((error) => { })
-  const data = res.entries.splice(0, 5)
-  await asset.save(data, 'json')
-  return data
+  const TV_KEY = process.env.TRAKT_API_KEY
+  const url = 'https://api.trakt.tv/users/shadybrothers/history/shows'
+  const res = await EleventyFetch(url, {
+    duration: '1h',
+    type: 'json',
+    fetchOptions: {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': 2,
+        'trakt-api-key': TV_KEY,
+      },
+    },
+  })
+  const shows = res
+  return shows.slice(0, 6)
 }
