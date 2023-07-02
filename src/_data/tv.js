a176e5dd2fdb8ed4a965dd943e6476f140b03443
flamedfury <heavy.job8629@fastmail.com>
2023-07-02 13:00:00 +1200

    data: update trakt username for tv.js and movies.js

--- src/_data/tv.js
+++ src/_data/tv.js
@@ -3,7 +3,7 @@ const EleventyFetch = require('@11ty/eleventy-fetch')
 module.exports = async function () {
   const TV_KEY = process.env.TRAKT_API_KEY
   const TMDB_KEY = process.env.TMDB_API_KEY
-  const url = 'https://api.trakt.tv/users/shadybrothers/history/shows'
+  const url = 'https://api.trakt.tv/users/flamed/history/shows'
   const res = await EleventyFetch(url, {
     duration: '1h',
     type: 'json',
@@ -29,4 +29,4 @@ module.exports = async function () {
   }
 
   return shows
-}
+} 
