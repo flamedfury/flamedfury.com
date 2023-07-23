f12faf9743f0c008af1958ad8029d88bc47916d0
flamedfury <heavy.job8629@fastmail.com>
2023-07-23 16:01:42 +1200

    raiderio.js: mucking around trying to get this to work

--- src/_data/raiderio.js
+++ src/_data/raiderio.js
@@ -16,9 +16,13 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-  let url = "https://raider.io/api/v1/characters/profile?region=us&realm=khaz-goroth&name=lunchcuttah&fields=gear%2Cguild%2Craid_progression_mythic_plus_scores_by_season%2Cmythic_plus_ranks%2Cmythic_plus_best_runs%2Cmythic_plus_recent_runs%2Cmythic_plus_highest_level_runs%2Craid_achievement_meta%2Craid_achievement_curve"
-  return EleventyFetch(url, {
-    duration: "1d", // save for 1 day
+  let url = "https://raider.io/api/v1/characters/profile?region=us&realm=khaz-goroth&name=lunchcuttah&fields=guild%2Cgear%2Craid_progression%2Craid_achievement_curve%2Cmythic_plus_scores_by_season%3Aseason-df-2mythic_plus_scores_by_season%3Aseason-df-1";
+  let response = await EleventyFetch(url, {
+    duration: "1h", // save for 1 hour
     type: "json"    // we’ll parse JSON for you
   });
+
+  return {
+    raiderio: response // pass the response data to the template
+  };
 };
