cf257d6f6092fdb6f90853dc67e80d4197ecf68c
flamedfury <heavy.job8629@fastmail.com>
2023-09-14 10:44:08 +1200

    update: update bookmarks function and layout

--- src/_data/bookmarks.js
+++ src/_data/bookmarks.js
@@ -1,14 +1,15 @@
 const EleventyFetch = require('@11ty/eleventy-fetch');
+const slugify = require('slugify');
 
 module.exports = async function () {
   let url = 'https://raw.githubusercontent.com/flamedfury/metadata-library/main/_data/bookmarks.json'
 
-  // Returning promise
+  // returning promise
+
   let data = await EleventyFetch(url, {
-    duration: '0s',
+    duration: '1h',
     type: 'json'
   });
 
-  // Export the data
   return data;
 };
