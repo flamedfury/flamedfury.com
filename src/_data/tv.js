56823617b0a0da2ad2a86d3b2e1964c6151fafcb
flamedfury <heavy.job8629@fastmail.com>
2023-10-05 21:50:34 +1300

    add filter to tv.js to exclude masterchef

--- src/_data/tv.js
+++ src/_data/tv.js
@@ -1,5 +1,8 @@
 const EleventyFetch = require('@11ty/eleventy-fetch')
 
+// Add MasterChef Australia to the denylist
+const denylist = ['MasterChef Australia'];
+
 module.exports = async function () {
   const TV_KEY = process.env.TRAKT_API_KEY
   const TMDB_KEY = process.env.TMDB_API_KEY
@@ -15,18 +18,27 @@ module.exports = async function () {
       },
     },
   })
-  const shows = res.slice(0, 4)
 
-  for (const show of shows) {
-    const tmdbId = show.show.ids.tmdb
-    const tmdbUrl = `https://api.themoviedb.org/3/tv/${tmdbId}?api_key=${TMDB_KEY}`
-    const tmdbRes = await EleventyFetch(tmdbUrl, {
-      duration: '1h',
-      type: 'json',
-    })
-    const posterPath = tmdbRes.poster_path
-    show.posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`
+  let shows = [];
+  for (const show of res) {
+    // Check if the show is not in the denylist
+    if (!denylist.includes(show.show.title)) {
+      const tmdbId = show.show.ids.tmdb
+      const tmdbUrl = `https://api.themoviedb.org/3/tv/${tmdbId}?api_key=${TMDB_KEY}`
+      const tmdbRes = await EleventyFetch(tmdbUrl, {
+        duration: '1h',
+        type: 'json',
+      })
+      const posterPath = tmdbRes.poster_path
+      show.posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`
+      shows.push(show);
+
+      // Break out of the loop if you have enough shows
+      if (shows.length >= 4) {
+        break;
+      }
+    }
   }
 
-  return shows
-} 
+  return shows;
+}
