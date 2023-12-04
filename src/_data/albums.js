c7fa25a92bae3b5040eebddcb646ea91dd05e1cf
flamedfury <heavy.job8629@fastmail.com>
2023-12-04 20:35:11 +1300

    test removing spotify/lastfm

--- src/_data/albums.js
+++ src/_data/albums.js
@@ -1,30 +1,30 @@
-const EleventyFetch = require('@11ty/eleventy-fetch');
-const { getSpotifyAccessToken } = require('./spotifyAuth');
-const { searchSpotifyAlbum } = require('./spotifySearch');
+// const EleventyFetch = require('@11ty/eleventy-fetch');
+// const { getSpotifyAccessToken } = require('./spotifyAuth');
+// const { searchSpotifyAlbum } = require('./spotifySearch');
 
-module.exports = async function () {
-  const API_KEY = process.env.LASTFM_KEY;
-  const USERNAME = process.env.LASTFM_USER;
-  const url = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${USERNAME}&api_key=${API_KEY}&limit=8&format=json&period=7day`;
+// module.exports = async function () {
+//   const API_KEY = process.env.LASTFM_KEY;
+//   const USERNAME = process.env.LASTFM_USER;
+//   const url = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${USERNAME}&api_key=${API_KEY}&limit=8&format=json&period=7day`;
 
-  const lastFmResponse = await EleventyFetch(url, {
-    duration: '1h',
-    type: 'json',
-  });
-  const albums = lastFmResponse.topalbums.album;
+//   const lastFmResponse = await EleventyFetch(url, {
+//     duration: '1h',
+//     type: 'json',
+//   });
+//   const albums = lastFmResponse.topalbums.album;
 
-  const spotifyAccessToken = await getSpotifyAccessToken();
+//   const spotifyAccessToken = await getSpotifyAccessToken();
 
-  const albumImages = await Promise.all(
-    albums.map(async (album) => {
-      const spotifyAlbum = await searchSpotifyAlbum(album.name, album.artist.name, spotifyAccessToken);
-      return spotifyAlbum.images.length > 0 ? spotifyAlbum.images[0].url : null;
-    })
-  );
+//   const albumImages = await Promise.all(
+//     albums.map(async (album) => {
+//       const spotifyAlbum = await searchSpotifyAlbum(album.name, album.artist.name, spotifyAccessToken);
+//       return spotifyAlbum.images.length > 0 ? spotifyAlbum.images[0].url : null;
+//     })
+//   );
 
-  albums.forEach((album, index) => {
-    album.image = albumImages[index];
-  });
+//   albums.forEach((album, index) => {
+//     album.image = albumImages[index];
+//   });
 
-  return albums;
-};
+//   return albums;
+// };
--- src/_data/spotifyAuth.js
+++ src/_data/spotifyAuth.js
@@ -1,26 +1,26 @@
-const axios = require('axios');
-require('dotenv').config({ systemvars: true });
+// const axios = require('axios');
+// require('dotenv').config({ systemvars: true });
 
-async function getSpotifyAccessToken() {
-  const authOptions = {
-    method: 'post',
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      Authorization: `Basic ${Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    data: 'grant_type=client_credentials',
-  };
+// async function getSpotifyAccessToken() {
+//   const authOptions = {
+//     method: 'post',
+//     url: 'https://accounts.spotify.com/api/token',
+//     headers: {
+//       Authorization: `Basic ${Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     data: 'grant_type=client_credentials',
+//   };
 
-  try {
-    const response = await axios(authOptions);
-    return response.data.access_token;
-  } catch (error) {
-    console.error('Error fetching Spotify access token:', error);
-    throw error;
-  }
-}
+//   try {
+//     const response = await axios(authOptions);
+//     return response.data.access_token;
+//   } catch (error) {
+//     console.error('Error fetching Spotify access token:', error);
+//     throw error;
+//   }
+// }
 
-module.exports = {
-  getSpotifyAccessToken,
-};
+// module.exports = {
+//   getSpotifyAccessToken,
+// };
