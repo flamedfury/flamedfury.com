15043b99524574a92a33e564ea4ed99c39254ced
flamedfury <heavy.job8629@fastmail.com>
2023-12-15 22:16:52 +1300

    now page: grouping episodes of the same tv show

--- src/_data/tv.js
+++ src/_data/tv.js
@@ -1,12 +1,27 @@
-const EleventyFetch = require('@11ty/eleventy-fetch')
+const EleventyFetch = require('@11ty/eleventy-fetch');
 
 // Add MasterChef Australia to the denylist
 const denylist = ['MasterChef Australia'];
 
+// Helper function to group episodes by show title
+function groupEpisodesByShow(episodes) {
+  const groupedShows = {};
+  episodes.forEach((episode) => {
+    const showTitle = episode.show.title;
+    if (!denylist.includes(showTitle)) {
+      if (!groupedShows[showTitle]) {
+        groupedShows[showTitle] = [];
+      }
+      groupedShows[showTitle].push(episode);
+    }
+  });
+  return groupedShows;
+}
+
 module.exports = async function () {
-  const TV_KEY = process.env.TRAKT_API_KEY
-  const TMDB_KEY = process.env.TMDB_API_KEY
-  const url = 'https://api.trakt.tv/users/flamed/history/shows'
+  const TV_KEY = process.env.TRAKT_API_KEY;
+  const TMDB_KEY = process.env.TMDB_API_KEY;
+  const url = 'https://api.trakt.tv/users/flamed/history/shows';
   const res = await EleventyFetch(url, {
     duration: '1h',
     type: 'json',
@@ -17,28 +32,61 @@ module.exports = async function () {
         'trakt-api-key': TV_KEY,
       },
     },
-  })
+  });
 
-  let shows = [];
+  let episodes = [];
   for (const show of res) {
-    // Check if the show is not in the denylist
-    if (!denylist.includes(show.show.title)) {
-      const tmdbId = show.show.ids.tmdb
-      const tmdbUrl = `https://api.themoviedb.org/3/tv/${tmdbId}?api_key=${TMDB_KEY}`
-      const tmdbRes = await EleventyFetch(tmdbUrl, {
-        duration: '1h',
-        type: 'json',
-      })
-      const posterPath = tmdbRes.poster_path
-      show.posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`
-      shows.push(show);
+    const tmdbId = show.show.ids.tmdb;
+    const tmdbUrl = `https://api.themoviedb.org/3/tv/${tmdbId}?api_key=${TMDB_KEY}`;
+    const tmdbRes = await EleventyFetch(tmdbUrl, {
+      duration: '1h',
+      type: 'json',
+    });
+    const posterPath = tmdbRes.poster_path;
+    show.posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+    episodes.push(show);
+  }
 
-      // Break out of the loop if you have enough shows
-      if (shows.length >= 4) {
-        break;
-      }
+  // Group episodes by show title
+  const groupedEpisodes = groupEpisodesByShow(episodes);
+
+  // Extract shows from grouped episodes with episode range and sort by most recent episode
+  const shows = [];
+  for (const showTitle in groupedEpisodes) {
+    if (Object.hasOwnProperty.call(groupedEpisodes, showTitle)) {
+      const episodes = groupedEpisodes[showTitle];
+      // Sort episodes by most recent episode (latest season and episode number)
+      episodes.sort((a, b) => {
+        if (a.episode.season === b.episode.season) {
+          return b.episode.number - a.episode.number;
+        }
+        return b.episode.season - a.episode.season;
+      });
+
+      // Determine the episode range
+      const episodeRange = episodes.length > 1 ?
+        `S${episodes[episodes.length - 1].episode.season}E${episodes[episodes.length - 1].episode.number} - S${episodes[0].episode.season}E${episodes[0].episode.number}` :
+        `S${episodes[0].episode.season}E${episodes[0].episode.number}`;
+
+      const showData = {
+        show: episodes[0].show,
+        posterUrl: episodes[0].posterUrl,
+        episodeRange: episodeRange,
+      };
+
+      shows.push(showData);
     }
   }
 
-  return shows;
-}
+  // Sort shows by the most recent episode
+  shows.sort((a, b) => {
+    // Compare the most recent episode for sorting
+    if (a.show.first_aired && b.show.first_aired) {
+      return new Date(b.show.first_aired) - new Date(a.show.first_aired);
+    }
+    // If the first aired date is missing, maintain the order
+    return 0;
+  });
+
+  return shows.slice(0, 4); // Adjust the limit of shows as per your requirement
+};
