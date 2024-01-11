c0b370f13369b878de341268230a3ac69cb40aac
flamedfury <heavy.job8629@fastmail.com>
2024-01-11 23:59:21 +1300

    adjust steam env variables

--- src/_data/games.js
+++ src/_data/games.js
@@ -4,9 +4,7 @@ require('dotenv').config({ systemvars: true });
 
 module.exports = async function () {
   try {
-    const API_KEY = process.env.STEAM_KEY;
-    const STEAMID = process.env.STEAM_ID;
-    const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${API_KEY}&steamid=${STEAMID}&format=json`;
+    const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${process.env.STEAM_KEY}&steamid=${process.env.STEAM_ID}&format=json`;
     const res = await EleventyFetch(url, {
       duration: '1h',
       type: 'json',
