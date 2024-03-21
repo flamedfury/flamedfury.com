b6ceb5f2382ab0b99345f5d31bf7921397c4ae10
flamedfury <heavy.job8629@fastmail.com>
2024-03-21 23:08:07 +1300

    update: slugify modification to replace / with -

--- config/utils/index.js
+++ config/utils/index.js
@@ -2,11 +2,16 @@ const slugify = require('slugify');
 
 /** Converts string to a slug form. */
 const slugifyString = str => {
-  return slugify(str, {
+  const slug = slugify(str, {
     replacement: '-',
     remove: /[#,&,+()$~%.'":*¿?¡!<>{}]/g,
     lower: true
   });
+
+  // Replace forward slashes with hyphens
+  const slugWithHyphens = slug.replace(/\//g, '-');
+  
+  return slugWithHyphens;
 };
 
 /** throw an error if the provided argument is not of the expected. */
