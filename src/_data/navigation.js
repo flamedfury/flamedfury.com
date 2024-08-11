0e16116be44d30fc2069c3116d05a8a7ea0b4d8f
flamedfury <heavy.job8629@fastmail.com>
2024-08-12 09:25:36 +1200

    update: navigation ESM support

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,74 +1,73 @@
-module.exports = {
-  top: [
-    {
-      text: 'Bookshelf',
-      url: '/bookshelf/'
-    },
-    {
-      text: 'Recordshelf',
-      url: '/recordshelf/'
-    },
-    {
-      text: 'Posts',
-      url: '/posts/'
-    },
-    {
-      text: 'Bookmarks',
-      url: '/bookmarks/'
-    },
-    {
-      text: 'Links',
-      url: '/links/'
-    },
-    {
-      text: 'Now',
-      url: '/now/'
-    },
-    {
-      text: 'About',
-      url: '/about/'
-    },
-    {
-      text: 'Feeds',
-      url: '/feeds/'
-    }
-  ],
-  bottom: [
-    {
-      text: 'Top',
-      url: '#top'
-    },
-    {
-      text: 'Contact',
-      url: '/contact/'
-    },
-    {
-      text: 'History',
-      url: '/history/'
-    },
-    {
-      text: 'Buttons',
-      url: '/buttons/'
-    },
-    {
-      text: 'Tags',
-      url: '/tags/'
-    },
-    {
-      text: 'PSA',
-      url: '/psa/'
-    },
-    {
-      text: 'SOA',
-      url: '/statement-of-audience/'
-    },
-    {
-      text: 'Privacy',
-      url: '/privacy/'
-    },
-    {
-      text: 'Styleguide',
-      url: '/styleguide/'
-    }
-  ]
-};
+export const top = [
+  {
+    text: 'Bookshelf',
+    url: '/bookshelf/'
+  },
+  {
+    text: 'Recordshelf',
+    url: '/recordshelf/'
+  },
+  {
+    text: 'Posts',
+    url: '/posts/'
+  },
+  {
+    text: 'Bookmarks',
+    url: '/bookmarks/'
+  },
+  {
+    text: 'Links',
+    url: '/links/'
+  },
+  {
+    text: 'Now',
+    url: '/now/'
+  },
+  {
+    text: 'About',
+    url: '/about/'
+  },
+  {
+    text: 'Feeds',
+    url: '/feeds/'
+  }
+];
+
+export const bottom = [
+  {
+    text: 'Top',
+    url: '#top'
+  },
+  {
+    text: 'Contact',
+    url: '/contact/'
+  },
+  {
+    text: 'History',
+    url: '/history/'
+  },
+  {
+    text: 'Buttons',
+    url: '/buttons/'
+  },
+  {
+    text: 'Tags',
+    url: '/tags/'
+  },
+  {
+    text: 'PSA',
+    url: '/psa/'
+  },
+  {
+    text: 'SOA',
+    url: '/statement-of-audience/'
+  },
+  {
+    text: 'Privacy',
+    url: '/privacy/'
+  },
+  {
+    text: 'Styleguide',
+    url: '/styleguide/'
+  }
+];
