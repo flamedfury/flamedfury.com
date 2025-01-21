9115f4f264fac6cb36601f748e6621f4f6c245bc
flamedfury <heavy.job8629@fastmail.com>
2025-01-21 21:48:19 +1300

    Prepend "./src" if not present

--- src/_config/shortcodes/image.js
+++ src/_config/shortcodes/image.js
@@ -21,6 +21,10 @@ export const imageShortcode = async (
   sizes = 'auto',
   formats = ['webp', 'jpeg']
 ) => {
+  // Prepend "./src" if not present
+  if (!src.startsWith('./src')) {
+    src = `./src${src}`;
+  }
   const metadata = await Image(src, {
     widths: [...widths],
     formats: [...formats],
