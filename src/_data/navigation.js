5d3cef7efb58af8791a173961ee07a136a493a72
flamedfury <heavy.job8629@fastmail.com>
2025-02-04 20:48:43 +1300

    update: new homepage hyperlinks

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,15 +1,15 @@
 export const top = [
   {
+    text: 'Posts',
+    url: '/posts/'
+  },
+  {
     text: 'Bookshelf',
     url: '/bookshelf/'
   },
   {
     text: 'Recordshelf',
     url: '/recordshelf/'
-  },
-  {
-    text: 'Posts',
-    url: '/posts/'
   },
   {
     text: 'Bookmarks',
@@ -28,8 +28,8 @@ export const top = [
     url: '/about/'
   },
   {
-    text: 'Feeds',
-    url: '/feeds/'
+    text: 'More',
+    url: '/#explore'
   }
 ];
 
@@ -43,18 +43,6 @@ export const bottom = [
     url: '/contact/'
   },
   {
-    text: 'History',
-    url: '/history/'
-  },
-  {
-    text: 'Buttons',
-    url: '/buttons/'
-  },
-  {
-    text: 'Tags',
-    url: '/tags/'
-  },
-  {
     text: 'PSA',
     url: '/psa/'
   },
@@ -65,9 +53,110 @@ export const bottom = [
   {
     text: 'Privacy',
     url: '/privacy/'
+  }
+];
+
+export const surfShelves = [
+  {
+    text: 'Bookshelf',
+    url: '/bookshelf/'
+  },
+  {
+    text: 'Comics',
+    url: '/comics/'
+  },
+  {
+    text: 'Gameshelf',
+    url: '/gameshelf/'
+  },
+  {
+    text: 'Mixtapes',
+    url: '/mixtapes/'
+  },
+  {
+    text: 'Recordshelf',
+    url: '/recordshelf/'
+  },
+  {
+    text: 'The Garage',
+    url: '/garage/'
+  }
+];
+
+export const surfSite = [
+  {
+    text: 'Archives',
+    url: '/archive/'
+  },
+  {
+    text: 'Buttons',
+    url: '/buttons/'
+  },
+  {
+    text: 'Changelog',
+    url: '/changelog/'
+  },
+  {
+    text: 'Feeds',
+    url: '/feeds/'
+  },
+  {
+    text: 'History',
+    url: '/history/'
+  },
+  {
+    text: 'Privacy',
+    url: '/privacy/'
+  },
+  {
+    text: 'PSA',
+    url: '/psa/'
+  },
+  {
+    text: 'SOA',
+    url: '/soa/'
   },
   {
     text: 'Styleguide',
     url: '/styleguide/'
+  },
+  {
+    text: 'Tags',
+    url: '/tags/'
+  }
+];
+
+export const surfPersonal = [
+  {
+    text: 'About',
+    url: '/about/'
+  },
+  {
+    text: 'Defaults',
+    url: '/defaults/'
+  },
+  {
+    text: 'Favourites',
+    url: '/favourites/'
+  },
+  {
+    text: 'Hello',
+    url: '/hello/'
+  },
+  {
+    text: 'Interests',
+    url: '/interests/'
+  },
+  {
+    text: 'Links',
+    url: '/links/'
+  },
+  {
+    text: 'Now',
+    url: '/now/'
+  },
+  {
+    text: 'Uses',
+    url: '/uses/'
   }
 ];
